e940fb51e4199bbcbae1ab66f6a662c4c4fc9f9a
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-26 15:58:43 +0300

    add profile model

--- wansentialsapp/models.py
+++ wansentialsapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    email = models.EmailField()
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+
