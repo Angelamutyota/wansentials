53bf5605cea0e837ceae104ecddd56b04af7f5fe
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-27 20:15:57 +0300

    add registration and login view

---
+++ wansentialsapp/forms.py
@@ -0,0 +1,17 @@
+from wansentialsapp.models import Profile
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name','profile_picture','bio','email','neighbourhood','location']
+        
--- wansentialsapp/views.py
+++ wansentialsapp/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html',)
+
+def registerPage(request):
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('loginpage') 
+        name = form.cleaned_data.get("username")
+        messages.success(request, 'Account was created for' , name)
+    context = {'form':form, 'profile':profile}
+    return render(request, 'accounts/register.html', context)
